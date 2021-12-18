595b7ca32e10cc74afde1a8cd2b18eedd4afe16d
Daisuke Tsukamoto <foo.yobina@gmail.com>
2021-12-19 04:02:52 +0900

    gensym関数を追加。
    動的にユニークなシンボルを生成する。

--- src/value/symbol.rs
+++ src/value/symbol.rs
@@ -2,6 +2,7 @@ use crate::value::*;
 use crate::context::{Context};
 use crate::ptr::*;
 use std::fmt::Debug;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
@@ -19,6 +20,8 @@ static SYMBOL_TYPEINFO: TypeInfo = new_typeinfo!(
     None,
 );
 
+static GENSYM_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
 impl NaviType for Symbol {
     fn typeinfo() -> NonNullConst<TypeInfo> {
         NonNullConst::new_unchecked(&SYMBOL_TYPEINFO as *const TypeInfo)
@@ -30,8 +33,14 @@ impl Symbol {
         std::ptr::eq(&SYMBOL_TYPEINFO, other_typeinfo)
     }
 
-    pub fn alloc(str: &String, ctx : &mut Context) -> FPtr<Symbol> {
-        string::NString::alloc_inner::<Symbol>(str, ctx)
+    pub fn alloc<T: Into<String>>(str: T, ctx : &mut Context) -> FPtr<Symbol> {
+        string::NString::alloc_inner::<Symbol>(&str.into(), ctx)
+    }
+
+    pub fn gensym<T :Into<String>>(name: T, ctx: &mut Context) -> FPtr<Symbol> {
+        let count = GENSYM_COUNTER.fetch_add(1, Ordering::SeqCst);
+        let name = name.into() + "_" + &count.to_string();
+        Self::alloc(&name, ctx)
     }
 
 }
